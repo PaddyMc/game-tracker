a34be56cc2b78b3d5dc9868743fb85021a0a4a8a
unknown <paddymchale@hotmail.com>
2018-10-17 23:07:09 +0100

    updated contract

--- contracts/GameTracker.sol
+++ contracts/GameTracker.sol
@@ -4,13 +4,13 @@ contract GameTracker {
 
     struct GameData {
         address owner;
-        bytes32 ipfsHash;
+        string ipfsHash;
     }
 
     mapping(uint256 => GameData) allGameData;
     uint256 number = 0;
 
-    function upload(bytes32 ipfsHash) public returns (bytes32) {
+    function upload(string ipfsHash) public returns (string) {
         //require(ipfsHash=> validate);
         GameData memory gamedata = GameData(msg.sender, ipfsHash);
         allGameData[number] = gamedata;
@@ -19,20 +19,17 @@ contract GameTracker {
         return ipfsHash;
     }
 
-    function getHashByNum(uint256 position) public returns (bytes32) {
+    function getHashByNum(uint256 position) public view returns (string) {
         //require(ipfsHash=> validate);
         GameData storage gamedata = allGameData[position];
-
-        log0(gamedata.ipfsHash);
         return gamedata.ipfsHash;
     }
 
-    function getNumberOfHashes() public returns (uint256) {
-        log0("asd");
+    function getNumberOfHashes() public view returns (uint256) {
         return number;
     }
 
-    // function getAllHashes() public returns (bytes32[]) {
+    // function getAllHashes() public view returns (bytes32[]) {
     //     log0("asd");
     //     bytes32[] memory hope;
     //     for ( uint i = 0; i < number; i++ ) {
